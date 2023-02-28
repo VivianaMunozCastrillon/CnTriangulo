56bf0d76af086e18dbeffa96f2483883547df12a
VivianaMuñozCastrillon <vivi84castri@gmail.com>
2023-02-28 13:54:06 -0500

    Agregar archivos de proyecto.

---
+++ CnTriangulo/ClsTriangulo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnTriangulo
+{
+    internal class ClsTriangulo
+    {
+        //Declaracion de variables
+        private double lado1;
+        private double lado2;
+        private double lado3;
+
+
+     //Metodos
+        public void Inicializar(double lado1, double lado2, double lado3)
+        {
+            this.lado1 = lado1;
+            this.lado2 = lado2;
+            this.lado3 = lado3;
+        }
+
+        
+        public void LadoMayor()
+        {
+            if (lado1 > lado2 & lado1 > lado3)
+            {
+                Console.WriteLine("El lado  mayor es:" + lado1);
+            }
+            else if (lado2 > lado1 & lado2 > lado3)
+            {
+                Console.WriteLine("El lado  mayor es:" + lado2);
+            }
+            else
+            {
+                Console.WriteLine("El lado  mayor es:" + lado3);
+            }
+        }
+
+        public bool Equilatero()
+        {
+            if (lado1 == lado2 && lado1 == lado3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ CnTriangulo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnTriangulo
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Nombre:Viviana Muñoz
+        /// Fecha:26/02/2023
+        /// Descripcion:Programa que solicita los tres lados de un triangulo e imprime el lado mayor y si es equilatero.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            //Instancia de clase
+            ClsTriangulo triangulo = new ClsTriangulo();
+
+            //Solicita datos
+            double lado1, lado2, lado3;
+            Console.Write("Ingrese el lado 1: ");
+            lado1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el lado 2: ");
+            lado2 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el lado 3: ");
+            lado3 = double.Parse(Console.ReadLine());
+
+            //Llamada de metodos
+            triangulo.Inicializar(lado1, lado2, lado3);
+            triangulo.LadoMayor();
+
+            //Condicion
+            if (triangulo.Equilatero())
+            {
+                Console.WriteLine("El triángulo es equilátero.");
+            }
+            else
+            {
+                Console.WriteLine("El triángulo no es equilátero.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
